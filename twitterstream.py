fa1e6917c18d0fe7d923d6891bf99832b277c523
S Anand <root.node@gmail.com>
2015-06-23 12:23:45 +0530

    ENH: log information to file and console

--- twitterstream.py
+++ twitterstream.py
@@ -6,20 +6,15 @@ import aiohttp
 import logging
 import psycopg2
 import oauthlib.oauth1
-from collections import namedtuple, Counter
 from urllib.parse import urlencode
+from collections import namedtuple, Counter
+from logging.handlers import RotatingFileHandler
 
 
+logger = logging.getLogger('twitterstream')
 queue = asyncio.Queue()
 Event = namedtuple('Event', ['run_id', 'data'])
-
-
-def serialize(args, keys=('follow', 'track', 'locations')):
-    return urlencode([
-        (key, ','.join(sorted(args[key])))
-        for key in keys
-        if args.get(key)
-    ])
+total_count = Counter()
 
 
 @asyncio.coroutine
@@ -37,19 +32,19 @@ def stream_tweets(oauth, run_id, data):
         if response.status in {420, 429} or response.status >= 500:
             backoff = backoff * 2 if backoff else 60 if response.status == 420 else 5
             result = yield from response.read()
-            logging.warning('HTTP %d: %ss retry: %s', response.status, backoff, result)
+            logger.warning('HTTP %d: %ss retry: %s', response.status, backoff, result)
             response.close()
             yield from asyncio.sleep(backoff)
         # For non-recoverable errors, just give up
         elif response.status != 200:
             result = yield from response.read()
-            logging.error('HTTP %d: %s', response.status, result)
+            logger.error('HTTP %d: %s', response.status, result)
             response.close()
             return
         else:
             connection = True
 
-    logging.info('Connected:%s', run_id)
+    logger.info('Connected:%s', run_id)
     try:
         reader = response.content
         while not reader.at_eof():
@@ -57,9 +52,9 @@ def stream_tweets(oauth, run_id, data):
             if line.strip():
                 yield from queue.put(Event(run_id=run_id, data=line.decode('utf-8')))
             else:
-                logging.debug('Blank line:%s', run_id)
+                logger.debug('Blank line:%s', run_id)
     finally:
-        logging.info('Disconnect:%s', run_id)
+        logger.info('Disconnect:%s', run_id)
         response.close()
 
 
@@ -76,7 +71,8 @@ def save_tweets(db, sleep, table='tweets'):
             event = queue.get_nowait()
             counter[event.run_id] += 1
             tweets.append(db.cur.mogrify('(%s, %s)', (event.run_id, event.data)).decode('utf-8'))
-        logging.info('Saving:%s', str(counter))
+        total_count.update(counter)
+        logger.debug('Saving:%s', str(counter))
         db.cur.execute('INSERT INTO %s (run, tweet) VALUES ' % table + ','.join(tweets))
         db.conn.commit()
 
@@ -85,14 +81,19 @@ def save_tweets(db, sleep, table='tweets'):
 def run_streams(db, sleep, table='config'):
     db.cur.execute('CREATE TABLE IF NOT EXISTS %s (run_id text PRIMARY KEY, config jsonb)' % table)
     db.conn.commit()
+
     Run = namedtuple('Run', ['task', 'data'])
+    params = {'follow', 'track', 'locations'}
     runs = {}
     last_modified = 0
     while True:
         db.cur.execute('SELECT * FROM %s' % table)
         config = dict(db.cur.fetchall())
         for run_id, conf in config.items():
-            data = serialize(conf)
+            data = urlencode([
+                (key, ','.join(sorted(conf[key])))
+                for key in conf
+                if key in params])
             oauth = oauthlib.oauth1.Client(
                 conf['consumer_key'], conf['consumer_secret'],
                 conf['access_token'], conf['access_secret'])
@@ -100,7 +101,8 @@ def run_streams(db, sleep, table='config'):
                 if runs[run_id].data == data:
                     continue
                 runs[run_id].task.cancel()
-            logging.info('Running:%s', run_id)
+            logger.info('Running:%s:%s',
+                        run_id, {key: val for key, val in conf.items() if key in params and val})
             runs[run_id] = Run(data=data, task=loop.create_task(
                 stream_tweets(oauth=oauth, run_id=run_id, data=data)))
         # Cancel runs that no loger exist in config
@@ -110,11 +112,40 @@ def run_streams(db, sleep, table='config'):
         yield from asyncio.sleep(sleep)
 
 
+@asyncio.coroutine
+def show_count(sleep):
+    while True:
+        logger.info('Tweets:%s', total_count)
+        total_count.subtract(total_count)
+        yield from asyncio.sleep(sleep)
+
+
 if __name__ == '__main__':
     import yaml
 
     folder = os.path.dirname(os.path.realpath(__file__))
     setup = yaml.load(open(os.path.join(folder, 'config.yaml')))
+
+    # We'll log WARNINGS into max 2MB log files called twitterstream.log.
+    # Up to 10 archives will be created: twitterstream.log.1, twitterstream.log.2
+    if 'logfile' in setup:
+        handler = RotatingFileHandler(filename=setup['logfile'], maxBytes=2000000, backupCount=10)
+        handler.setLevel(logging.WARNING)
+        formatter = logging.Formatter('{asctime},{levelname:8s},"{message}"',
+                                      datefmt='%Y-%M-%d %H:%M:%S', style='{')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    # We'll log everything to console. Just redirect to /dev/null if you don't want
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('{asctime}:{name}:{levelname}:{message}',
+                                  datefmt='%H:%M:%S', style='{')
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+
+    # Set logging level to loglevel in config.yaml. Default to logging.INFO
+    logger.setLevel(getattr(logging, setup.get('loglevel', 'info').upper(), logging.INFO))
 
     conn = psycopg2.connect(
         database=setup.get('database'),
@@ -124,11 +155,13 @@ if __name__ == '__main__':
     DB = namedtuple('DB', ['conn', 'cur'])
     db = DB(conn=conn, cur=conn.cursor())
 
-    logging.basicConfig(level=getattr(logging, setup.get('loglevel', 'info').upper(), logging.INFO))
-
     loop = asyncio.get_event_loop()
     loop.create_task(save_tweets(db, sleep=setup.get('save_every', 1)))
     loop.create_task(run_streams(db, sleep=setup.get('reload_every', 10)))
-    logging.info('Started server')
+
+    count_every = setup.get('count_every', 60)
+    loop.create_task(show_count(sleep=count_every))
+    logger.info('Started server. Will update tweet count every %ds', count_every)
+
     loop.run_forever()
     loop.close()
